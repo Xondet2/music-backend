5f7f9b53659d03ba9f228ea661045a3985824288
Xondet2 <david.ordonezla@campusucc.edu.co>
2025-09-30 22:26:28 -0500

    Fix

--- reproductor/settings.py
+++ reproductor/settings.py
@@ -1,6 +1,7 @@
 import os
 import dj_database_url
 from pathlib import Path
+import dj_database_url
 
 # Rutas base
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,10 +55,7 @@ WSGI_APPLICATION = "music_backend.wsgi.application"
 
 # Configuración base: SQLite (local)
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 # Render: PostgreSQL
